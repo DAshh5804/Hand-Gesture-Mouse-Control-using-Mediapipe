17bdadc96206c16f63e9675342f6f929490ee487
dimensionalchaos <rajasohal04@gmail.com>
2025-01-03 19:47:22 +0530

    commit

--- hand_recognition.py
+++ hand_recognition.py
@@ -6,7 +6,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 class HandRecognition:
     def __init__(self, max_num_hands=1, min_detection_confidence=0.7):
-        self.hands = mp_hands.Hands(max_num_hands=max_num_hands, min_detection_confidence=min_detection_confidence)
+        self.h nds = mp_hands.Hands(max_num_hands=max_num_hands, min_detection_confidence=min_detection_confidence)
 
     def process_frame(self, frame):
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
